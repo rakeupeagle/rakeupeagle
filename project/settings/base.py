2470bd4f804c8cdd7d91b2c37f02bc38418cd20f
David Binetti <dbinetti@gmail.com>
2020-10-17 13:21:35 -0600

    add search path

--- project/settings/base.py
+++ project/settings/base.py
@@ -101,6 +101,10 @@ STATIC_ROOT = root('staticfiles')
 MEDIA_ROOT = root('mediafiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    root('app/static'),
+    root('static'),
+]
 
 # Bootstrap
 BOOTSTRAP4 = {
