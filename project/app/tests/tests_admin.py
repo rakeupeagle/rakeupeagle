4645c9622c3bd399c5180073afe6b32d1528255c
David Binetti <dbinetti@gmail.com>
2020-11-08 08:25:57 -0700

    update tests

--- project/app/tests/tests_admin.py
+++ project/app/tests/tests_admin.py
@@ -14,3 +14,15 @@ def test_index(admin_client):
     path = reverse('admin:index')
     response = admin_client.get(path)
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_recipients(admin_client):
+    path = reverse('admin:app_recipient_changelist')
+    response = admin_client.get(path)
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_volunteers(admin_client):
+    path = reverse('admin:app_volunteer_changelist')
+    response = admin_client.get(path)
+    assert response.status_code == 200
--- project/app/tests/tests_anon.py
+++ project/app/tests/tests_anon.py
@@ -1,8 +1,8 @@
 # Django
-from django.urls import reverse
-
-# First-Party
+# Third-Party
 import pytest
+
+from django.urls import reverse
 
 
 def test_deploy():
@@ -13,3 +13,8 @@ def test_index(anon_client):
     path = reverse('index')
     response = anon_client.get(path)
     assert response.status_code == 200
+
+# def test_recipients(anon_client):
+#     path = reverse('recipients')
+#     response = anon_client.get(path)
+#     assert response.status_code == 200
