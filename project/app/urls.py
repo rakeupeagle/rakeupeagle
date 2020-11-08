76cb124e3177f01489b5e6f1fdcd9e2568fe0e14
David Binetti <dbinetti@gmail.com>
2020-11-07 20:41:41 -0700

    Update urls.py

--- project/app/urls.py
+++ project/app/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('sitemap.txt', views.sitemap, name='sitemap',),
 
     # Dashboard
-    path('recipient/', views.recipients, name='recipients',),
-    path('confirmation/', views.confirmation, name='confirmation',),
+    # path('recipient/', views.recipients, name='recipients',),
+    # path('confirmation/', views.confirmation, name='confirmation',),
     # path('volunteer/', views.volunteers, name='volunteers',),
     # path('thanks/', views.thanks, name='thanks',),
     # path('volunteer/<volunteer_id>', views.volunteer, name='volunteer',),
