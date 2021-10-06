c5f039b0d2940828511829f9491d678a43b9e40a
David Binetti <dbinetti@gmail.com>
2021-10-06 08:45:56 -0600

    Update signals.py

--- project/app/signals.py
+++ project/app/signals.py
@@ -2,9 +2,11 @@ from django.db.models.signals import post_save
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
+from .models import Message
 from .models import User
 from .tasks import create_account_from_user
 from .tasks import delete_user
+from .tasks import send_text_from_message
 
 
 @receiver(post_save, sender=User)
@@ -15,5 +17,11 @@ def user_post_save(sender, instance, created, **kwargs):
 
 @receiver(pre_delete, sender=User)
 def pre_delete_user(sender, instance, **kwargs):
-    # delete_user(instance.username)
+    delete_user(instance.username)
+    return
+
+@receiver(post_save, sender=Message)
+def message_post_save(sender, instance, created, **kwargs):
+    if created and instance.direction == instance.DIRECTION.outbound:
+        send_text_from_message.delay(instance)
     return
