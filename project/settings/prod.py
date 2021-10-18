1a64e02628f1dff10ca270f34d68beac3cd61cbe
David Binetti <dbinetti@gmail.com>
2021-10-18 09:13:17 -0600

    Update prod.py

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -11,6 +11,8 @@ ALLOWED_HOSTS = [
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 SENDGRID_API_KEY = env("SENDGRID_API_KEY")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+SENDGRID_TRACK_CLICKS_HTML = False
+SENDGRID_TRACK_CLICKS_PLAIN = False
 
 # Sentry
 SENTY_RELEASE = env("HEROKU_SLUG_COMMIT")
