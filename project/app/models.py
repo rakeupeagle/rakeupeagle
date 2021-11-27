64993c5c2678ed141aa060b54c17eb4b66a264d0
David Binetti <dbinetti@gmail.com>
2021-11-27 08:36:06 -0700

    remove state from account

--- project/app/models.py
+++ project/app/models.py
@@ -42,17 +42,6 @@ class Account(PolymorphicModel):
     id = HashidAutoField(
         primary_key=True,
     )
-    STATE = Choices(
-        (-20, 'cancelled', 'Cancelled'),
-        (-10, 'exclude', 'Excluded'),
-        (0, 'new', 'New'),
-        (10, 'include', 'Included'),
-        (20, 'confirmed', 'Confirmed'),
-    )
-    state = FSMIntegerField(
-        choices=STATE,
-        default=STATE.new,
-    )
     name = models.CharField(
         max_length=100,
         blank=True,
@@ -189,6 +178,94 @@ class Recipient(models.Model):
         return
 
 
+# class RecipientAccount(Account):
+#     id = HashidAutoField(
+#         primary_key=True,
+#     )
+#     STATE = Choices(
+#         (-20, 'cancelled', 'Cancelled'),
+#         (-10, 'exclude', 'Excluded'),
+#         (0, 'new', 'New'),
+#         (10, 'include', 'Included'),
+#         (20, 'confirmed', 'Confirmed'),
+#     )
+#     state = FSMIntegerField(
+#         choices=STATE,
+#         default=STATE.new,
+#     )
+#     SIZE = Choices(
+#         (110, 'small', 'Small (1-15 bags)'),
+#         (120, 'medium', 'Medium (16-30 bags)'),
+#         (130, 'large', 'Large (31+ bags)'),
+#     )
+#     size = models.IntegerField(
+#         blank=True,
+#         choices=SIZE,
+#         help_text="""Please provide the approximate yard size."""
+#     )
+#     location = models.CharField(
+#         max_length=512,
+#         blank=True,
+#         default='',
+#         help_text="""Please provide the location address to be raked."""
+#     )
+#     place = models.CharField(
+#         max_length=255,
+#         blank=True,
+#         default='',
+#     )
+#     is_precise = models.BooleanField(
+#         default=False,
+#     )
+#     point = models.PointField(
+#         null=True,
+#         blank=True,
+#     )
+#     geocode = models.JSONField(
+#         blank=True,
+#         null=True,
+#     )
+#     is_dog = models.BooleanField(
+#         blank=True,
+#         default=False,
+#         help_text=mark_safe("""If you have a dog, it must be contained in your home for us to rake. <em>Also, you must clean up all animal waste before we arrive or our team group will not be able to rake.</em>"""),
+#     )
+#     notes = models.TextField(
+#         max_length=2000,
+#         blank=True,
+#         default='',
+#         help_text="""Please add any other notes you think we should know.""",
+#     )
+#     admin_notes = models.TextField(
+#         max_length=2000,
+#         blank=True,
+#         default='',
+#         help_text="""Administrator Notes (from calling).""",
+#     )
+#     bags = models.IntegerField(
+#         blank=True,
+#         null=True,
+#         help_text='Actual Bags Used',
+#     )
+#     hours = models.FloatField(
+#         blank=True,
+#         null=True,
+#         help_text='Actual Hours Worked',
+#     )
+#     created = models.DateTimeField(
+#         auto_now_add=True,
+#     )
+#     updated = models.DateTimeField(
+#         auto_now=True,
+#     )
+#     def __str__(self):
+#         return f"{self.name}, {self.location} - {self.get_size_display()}"
+
+#     @transition(field=state, source=[STATE.pending,STATE.new], target=STATE.confirmed)
+#     def confirm(self):
+#         return
+
+
 # @reversion.register()
 class Team(models.Model):
     id = HashidAutoField(
@@ -281,6 +358,78 @@ class Team(models.Model):
     @transition(field=state, source=[STATE.pending,STATE.new,], target=STATE.confirmed)
     def confirm(self):
         return
+
+
+# class TeamAccount(Account):
+#     id = HashidAutoField(
+#         primary_key=True,
+#     )
+#     STATE = Choices(
+#         (-20, 'cancelled', 'Cancelled'),
+#         (-10, 'exclude', 'Excluded'),
+#         (0, 'new', 'New'),
+#         (10, 'include', 'Included'),
+#         (20, 'confirmed', 'Confirmed'),
+#     )
+#     state = FSMIntegerField(
+#         choices=STATE,
+#         default=STATE.new,
+#     )
+#     SIZE = Choices(
+#         (105, 'solo', 'Solo (1 Adult)'),
+#         (110, 'xs', 'Extra-Small (2-5 Adults)'),
+#         (120, 'small', 'Small (6-10 Adults)'),
+#         (130, 'medium', 'Medium (11-15 Adults)'),
+#         (140, 'large', 'Large (16-20 Adults)'),
+#         (150, 'xl', 'Extra-Large (21+ Adults)'),
+#     )
+#     size = models.IntegerField(
+#         blank=False,
+#         choices=SIZE,
+#         help_text='The size of your group. (Number of adults, or equivalent in children.)',
+#     )
+#     team = models.CharField(
+#         max_length=100,
+#         blank=True,
+#         default='',
+#         help_text="""Whimsical Team Name (ie, Robert's Rakers, Covey Courters, etc.)""",
+#     )
+#     reference = models.CharField(
+#         max_length=512,
+#         blank=True,
+#         default='',
+#         help_text="""How did you hear about us?""",
+#     )
+#     actual = models.IntegerField(
+#         blank=True,
+#         null=True,
+#         help_text='The actual number of adults, or adult-equivalent in children.',
+#     )
+#     notes = models.TextField(
+#         max_length=512,
+#         blank=True,
+#         default='',
+#         help_text="""Notes.""",
+#     )
+#     admin_notes = models.TextField(
+#         max_length=2000,
+#         blank=True,
+#         default='',
+#         help_text="""Administrator Notes (from calling).""",
+#     )
+#     created = models.DateTimeField(
+#         auto_now_add=True,
+#     )
+#     updated = models.DateTimeField(
+#         auto_now=True,
+#     )
+
+#     def __str__(self):
+#         return f"{self.team} - {self.get_size_display()}"
+
+#     @transition(field=state, source=[STATE.pending,STATE.new,], target=STATE.confirmed)
+#     def confirm(self):
+#         return
 
 
 class Event(models.Model):
