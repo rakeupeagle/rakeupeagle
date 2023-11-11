14fe5b458963e11dead3befad592c8c7b066cda6
David Binetti <dbinetti@gmail.com>
2023-11-10 23:20:15 -0700

    str rep

--- project/app/models.py
+++ project/app/models.py
@@ -343,7 +343,7 @@ class Yard(models.Model):
         auto_now=True,
     )
     def __str__(self):
-        return f"{self.recipient.user.name} - {self.recipient.get_size_display()}"
+        return f"{self.event.year} - {self.recipient.user.name} - {self.recipient.get_size_display()}"
 
 
 class Rake(models.Model):
@@ -391,7 +391,7 @@ class Rake(models.Model):
         auto_now=True,
     )
     def __str__(self):
-        return f"{self.team.user.name} - {self.team.get_size_display()}"
+        return f"{self.year} - {self.team.user.name} - {self.team.get_size_display()}"
 
 
 class Event(models.Model):
