2037af55ef3db9cfdc91d4cfce80c0ba01496e3f
David Binetti <dbinetti@gmail.com>
2023-11-13 09:06:29 -0700

    remove rake, yard models

--- project/app/inlines.py
+++ project/app/inlines.py
@@ -8,22 +8,20 @@ from .models import Conversation
 from .models import Message
 from .models import MessageArchive
 from .models import Participant
-from .models import Rake
 from .models import Receipt
 from .models import Recipient
 from .models import Team
-from .models import Yard
 
 
 class AssignmentInline(admin.TabularInline):
     model = Assignment
     fields = [
-        'yard',
-        'rake',
+        'recipient',
+        'team',
     ]
     autocomplete_fields = [
-        'yard',
-        'rake',
+        'recipient',
+        'team',
     ]
     extra = 0
     show_change_link = True
@@ -34,33 +32,6 @@ class TeamInline(admin.TabularInline):
     model = Team
     fields = [
         'size',
-    ]
-    autocomplete_fields = [
-    ]
-    extra = 0
-    show_change_link = True
-
-
-class YardInline(admin.TabularInline):
-    model = Yard
-    fields = [
-        'recipient',
-        'state',
-        'event',
-    ]
-    autocomplete_fields = [
-
-    ]
-    extra = 0
-    show_change_link = True
-
-
-class RakeInline(admin.TabularInline):
-    model = Rake
-    fields = [
-        'team',
-        'state',
-        'event',
     ]
     autocomplete_fields = [
     ]
