6551d0783d3f18b1eed3b89e1098f059025e0c99
David Binetti <dbinetti@gmail.com>
2024-09-14 07:17:31 -0600

    fix messages formatting

--- project/settings/base.py
+++ project/settings/base.py
@@ -1,5 +1,6 @@
 import logging.config
 
+from django.contrib.messages import constants as messages
 from django.utils.log import DEFAULT_LOGGING
 from environ import Env
 from environ import Path
@@ -113,50 +114,15 @@ PHONENUMBER_DB_FORMAT = 'NATIONAL'
 PHONENUMBER_DEFAULT_REGION = 'US'
 
 # Sentry
-
 SENTRY_DSN = env("SENTRY_DSN")
 SENTRY_ENVIRONMENT = env("SENTRY_ENVIRONMENT")
 SENTRY_CONFIG = {
     'send_default_pii': True,
 }
 
-# Bootstrap
-BOOTSTRAP4 = {
-    "css_url": {
-        "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
-        "integrity": "sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk",
-        "crossorigin": "anonymous",
-    },
-    "javascript_url": {
-        "url": "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js",
-        "integrity": "sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI",
-        "crossorigin": "anonymous",
-    },
-    "jquery_url": {
-        "url": "https://code.jquery.com/jquery-3.5.1.min.js",
-        "integrity": "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=",
-        "crossorigin": "anonymous",
-    },
-    "jquery_slim_url": {
-        "url": "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-        "integrity": "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj",
-        "crossorigin": "anonymous",
-    },
-    "popper_url": {
-        "url": "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
-        "integrity": "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo",
-        "crossorigin": "anonymous",
-    },
-    "theme_url": None,
-    'javascript_in_head': True,
-    'include_jquery': 'slim',
-}
-
-# Font Awesome
-DJANGO_ICONS = {
-    "ICONS": {
-        "settings": {"name": "far fa-gear"},
-    },
+# Bootstrap override
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
 }
 
 # Middleware
