2266db63d5ff748cb338990eb36163f0deab5f8f
David Binetti <dbinetti@gmail.com>
2024-10-01 10:38:48 -0600

    change storage backend

--- project/settings.py
+++ project/settings.py
@@ -101,7 +101,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
