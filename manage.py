dbf7e0fadb59dfc42cb31b478db9ea370baae40c
David Binetti <dbinetti@gmail.com>
2024-10-01 09:38:17 -0600

    still missing prod

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
